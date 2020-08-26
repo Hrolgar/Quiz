54a020542841e5a883da36bf6c42183504958ac8
Hrolgar <helgiskafta@gmail.com>
2020-08-26 02:20:03 +0200

    quick fix in popup

--- popup.js
+++ popup.js
@@ -1,15 +1,14 @@
-const popup = document.querySelector(".popup");
-const popupWrap = document.querySelector(".popup-wrapper");
+const popup = document.querySelector(".popup-wrapper");
 const close = document.querySelector(".popup-close");
 
 setTimeout(() => {
-  popupWrap.style.display = "block";
+  popup.style.display = "block";
 }, 5000);
 
 popup.addEventListener("click", (e) => {
-  popupWrap.style.display = "none";
+  popup.style.display = "none";
 });
 
 close.addEventListener("click", (e) => {
-  popupWrap.style.display = "none";
+  popup.style.display = "none";
 });
